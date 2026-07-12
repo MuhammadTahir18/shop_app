83312d9719bb21016ce32c98745941e305dcc4f4
MuhammadTahir-D <mtahir98888@gmail.com>
2026-07-12 01:44:47 -0700

    Created project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shop_app/core/theme/app_theme.dart';
+
+
+import 'core/theme/app_theme.dart';
 
 void main() {
   runApp(
